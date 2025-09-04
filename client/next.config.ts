c827b053205b0140986949b7a227c2e513a595c4
Issaoui-Ahmed <aissaoui2022@gmail.com>
2025-09-04 11:44:45 -0400

    Cleaned root directory / client app webflow setyp

--- client/next.config.ts
+++ client/next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from 'next'
 import path from 'path'
 
 const nextConfig: NextConfig = {
+  basePath: '/client',
+  assetPrefix: '/client',
   output: 'export', // Outputs a Single-Page Application (SPA)
   distDir: 'build', // Changes the build output directory to `build`
   outputFileTracingRoot: path.join(__dirname, '..'),
